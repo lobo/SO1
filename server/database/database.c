a2acc3a57b32d08fae3b0ed25ecfd604e9f725aa
Jere <jaagaard@itba.edu.ar>
2016-05-01 17:28:20 -0300

    Empece con el manejo de la base de datos, metodo de registro de usuario implementado

--- server/database/database.c
+++ server/database/database.c
@@ -1,5 +1,6 @@
 #include <sqlite3.h>
 #include <stdio.h>
+#include <string.h>
 #include "database.h"
 #include "./sqlite3/sqlite3.h"
 
@@ -33,6 +34,52 @@
     }\
 }
 
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+const char* db_file = "DB/chatroom.db";
+
+static int callback(void* NotUsed, int argc, char** argv, char** column_name) {
+    int i;
+    for (i = 0 ; i < argc ; i++) {
+        printf("%s = %s\n", column_name[i], argv[i] ? argv[i] : "NULL");
+    }
+    putchar('\n');
+    return 0;
+}
+
+static int register_user (char* username, char* password) {
+    sqlite3* db;
+    int rc;
+    char* sql;
+    char* errMsg = 0;
+
+    rc = sqlite3_open(db_file, &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open DB file: %s\n", sqlite3_errmsg(db));
+        exit(0);
+    } else {
+        fprintf(stdout, "Opened DB successfully\n");
+    }
+
+    sprintf(sql, "INSERT INTO USERNAME(USER, PASSWORD) VALUES ('%s', '%s');", username, password);
+
+    rc = sqlite3_exec(db, sql, callback, 0, &errMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Couldn't register your username. Error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+    } else {
+        fprintf(stdout, "Username %s registered successfully\n", username);
+    }
+
+    sqlite3_close(db);
+
+    return rc;
+}
 
 void db_close() {
     sqlite3_close(db);
