aa50ecc6e9962f48a33fc8761d8d6e3dd19e89ec
Daniel Lobo <dlobo@itba.edu.ar>
2016-05-14 20:01:33 -0300

    Up logger

--- logger/log_daemon.c
+++ logger/log_daemon.c
@@ -6,7 +6,7 @@
 #include <sys/msg.h>
 
 #define MAX_SIZE 200
-#define LOGS_FILE "server-logs.txt"
+#define LOGFILE_PATH "../logger/logs.txt"
 
 struct msgbuf {
 	long mtype;
@@ -18,28 +18,23 @@ int main(void)
 {
     struct msgbuf buf;
     int msqid;
-    key_t key;
-
-    if ((key = ftok("msgq.c", 'B')) == -1) {  
-        perror("ftok fallo");
-        exit(1);
-    }
+    key_t key = 123;
 
     if ((msqid = msgget(key, 0644)) == -1) { 
         perror("msgget error: fallo conectarse a la cola.");
         exit(1);
     }
     
-    printf("Listo para recibir mensajes.\n");
+    //printf("Listo para recibir mensajes.\n");
 
-    for(;;) { // corre siempre
+    while(1) { // corre siempre
         if (msgrcv(msqid, &buf, sizeof(buf.message), 0, 0) == -1) {
             perror("msgrcv");
             exit(1);
         }
 
         printf("El mensaje es: \"%s\"\n", buf.message);
-        FILE * fp = fopen(LOGS_FILE, "ab");
+        FILE * fp = fopen(LOGFILE_PATH, "ab");
 	    if (fp != NULL)
 	    {
 	        fputs(buf.message, fp);
